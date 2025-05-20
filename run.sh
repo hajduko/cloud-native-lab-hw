11ada191d33920aabc7c44a1d660a750989f1ad1
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2025-05-21 01:50:23 +0200

    Changed run and test.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 
-# echo "Cleaning up previous deployments..."
-# sudo pkill -f 'kubectl port-forward'
-# sudo kubectl delete all --all
+echo "Installing config..."
+echo ""
 
-echo "Starting the setup..."
 sudo kubectl apply -f config.yaml
 
+echo ""
+echo "Done installing config"
+
+echo ""
 echo "Building and importing Docker images..."
+echo ""
+
 sudo docker build --no-cache -t imagegrab:latest ./1_imagegrab
 sudo docker save -o /tmp/imagegrab.tar imagegrab:latest
 sudo ctr images import /tmp/imagegrab.tar
@@ -34,32 +38,67 @@ sudo docker save -o /tmp/tag.tar tag:latest
 sudo ctr images import /tmp/tag.tar
 sudo rm /tmp/tag.tar
 
+echo ""
+echo "Imported images into containerd"
+echo ""
 echo "Deploying Redis and MinIO..."
+echo ""
+
 sudo kubectl apply -f redis.yaml
 sudo kubectl apply -f minio.yaml
 
+echo ""
+echo "Deployed Redis and MinIO"
+echo ""
 echo "Deploying services..."
+echo ""
+
 sudo kubectl apply -f 1_imagegrab/imagegrab_deployment.yaml
 sudo kubectl apply -f 2_resize/resize_deployment.yaml
 sudo kubectl apply -f 3_grayscale/grayscale_deployment.yaml
 sudo kubectl apply -f 4_objectdetect/objectdetect_deployment.yaml
 sudo kubectl apply -f 5_tag/tag_deployment.yaml
 
+echo ""
+echo "Deployed services"
+echo ""
 echo "Installing MinIO client..."
+echo ""
+
 sudo curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
 sudo chmod +x mc
 sudo mv mc /usr/local/bin/
 
+echo ""
+echo "MinIO client installed"
+echo ""
 echo "Setting up port forwarding..."
+echo ""
+
 sleep 10  # Wait for services to be up
 sudo kubectl port-forward service/imagegrab 8080:80 &
 sudo kubectl port-forward service/minio 9000:9000 &
 
-# echo "Setting up MinIO bucket..."
-# sudo mc alias set localminio http://localhost:9000 minioadmin minioadmin
-# sudo mc mb localminio/images
+echo ""
+echo "Port forwarding set up"
+echo ""
+echo "Setting up MinIO bucket..."
+echo ""
 
-# echo "Setting up testing..."
-# sudo chmod +x test.sh
+sudo mc alias set localminio http://localhost:9000 minioadmin minioadmin
+sudo mc mb localminio/images
+
+echo ""
+echo "Done MinIO bucket setup"
+echo ""
+echo "Setting up testing..."
+echo ""
+sudo chmod +x test.sh
+sudo git config --global user.name "hajduko"
+sudo git config --global user.email "hajdu.kolos@edu.bme.hu"
+ssh-keygen -t ed25519 -C "hajdu.kolos@edu.bme.hu"
+cat ~/.ssh/id_ed25519.pub
+
+read -p "Copy the above key to your GitHub SSH keys (https://github.com/settings/keys) and press enter to continue..."
 
 echo "All services deployed!"
--- test.sh
+++ test.sh
@@ -2,3 +2,12 @@ echo "Testing the setup..."
 sudo curl -X POST http://localhost:8080/upload -F "image=@test-1.jpg"
 sleep 5
 sudo mc ls --recursive localminio/images
+read -p "Enter the uid of the images: " uid
+sudo mc get localminio/images/$uid/original.jpg original.jpg
+sudo mc get localminio/images/$uid/resized.jpg resized.jpg
+sudo mc get localminio/images/$uid/grayscale.jpg grayscale.jpg
+sudo mc get localminio/images/$uid/result.jpg result.jpg
+sudo git add original.jpg resized.jpg grayscale.jpg result.jpg
+sudo git commit -m "Test images"
+sudo git push
+echo "Test images pushed to GitHub"
