e5c822ae28182216d48c8d0b93e83feb06faa7e9
Hajdu Kolos <hajdu.kolos@edu.bme.hu>
2025-05-20 21:43:48 +0200

    Changed run.sh and test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 echo "Testing the setup..."
 sudo curl -X POST http://localhost:8080/upload -F "image=@test-1.jpg"
-sudo uid=$(mc ls localminio/images | awk '{print $5}' | tr -d '/')
-sudo mc ls localminio/images/$uid
+sleep 5
+sudo mc ls --recursive localminio/images
